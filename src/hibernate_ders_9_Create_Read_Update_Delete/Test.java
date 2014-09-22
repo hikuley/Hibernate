b47e4965a2c1a6fd8370173c2e16026f5e3d9d52
Halil İbrahim Küley <ssoulless25@gmail.com>
2014-09-22 22:44:23 +0300

    CRUD

--- src/hibernate_ders_9_Create_Read_Update_Delete/Test.java
+++ src/hibernate_ders_9_Create_Read_Update_Delete/Test.java
@@ -1,9 +1,34 @@
 package hibernate_ders_9_Create_Read_Update_Delete;
 
+import config.HiberateConfig;
+import org.hibernate.Session;
+
 /**
  * Created by halil on 21.09.2014.
  */
 public class Test {
 
+    public static void main(String[] args) {
+        Session session = HiberateConfig.getSession();
+        session.beginTransaction();
 
+//        for (int i = 0; i < 20; i++) {
+//            Users users = new Users("Halil ibrahim" + i, "Kuley" + i);
+//            session.save(users);
+//        }
+
+//        Users user = (Users) session.get(Users.class, 5); //Kayıdı seçtik getirdik.
+//        user.setAd("Name Updated");
+//        user.setSoyad("Surname Updated");
+//        session.update(user); // kayıtı güncelledik bu bu satırı aslında yazmamıza bile gerek yoktur.
+
+//        Users user = (Users) session.get(Users.class, 15); // nesnemizi seçtik
+//        session.delete(user); // daha sonra nesnemizi sildik
+
+//        Users users = new Users(); // bir nesne kendimiz oluşturduk
+//        users.setId(5);//nesnemizi delete methoduna verdik ve sildik.
+//        session.delete(users);
+
+        session.getTransaction().commit();
+    }
 }
--- src/hibernate_ders_9_Create_Read_Update_Delete/Users.java
+++ src/hibernate_ders_9_Create_Read_Update_Delete/Users.java
@@ -1,7 +1,51 @@
 package hibernate_ders_9_Create_Read_Update_Delete;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by halil on 21.09.2014.
  */
+@Entity
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String ad;
+    private String soyad;
+
+    public Users() {
+    }
+
+    public Users(String ad, String soyad) {
+        this.ad = ad;
+        this.soyad = soyad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
 }
